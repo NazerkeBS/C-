05479acc2464652e3d7bea556bb2685484ad66af
Nazerke Seidan <33206532+NazerkeBS@users.noreply.github.com>
2018-04-01 21:06:31 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,67 +1,109 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
-// to use private variables of another class: friend
+int longestheads(const string& seq)
+{
+    int cnt;
+    int max=-1;
+    for(unsigned int i=0; i<seq.size(); i++)
+    {
 
-class Rational{
-private:
-    int num;
-    int den;
-public:
-    Rational(int num=0, int den=1){    // default constructor
-    this->num=num;
-    if(den != 0){
-    this->den=den;
+        if(seq[i]=='H')
+        {
+            cnt=0;
+            while(seq[i]=='H')
+            {
+                cnt++;
+                i++;
+            }
+            if(max< cnt)
+            {
+                max=cnt;
+            }
+        }
+
     }
-    }
-    Rational(const Rational& other) : num(other.num),den(other.den){
-    // copy constructor
-    }
-    Rational& operator=(const Rational& other){
-    // assignment
-    num=other.num;
-    den=other.den;
-    return *this;
-    }
-
-    Rational operator*(const Rational& right){
-    Rational result(num*right.num, den*right.den);
-    return result;
-    }
-
-    Rational operator+(const Rational&right){
-    Rational result(num*right.den+den*right.num, den*right.den);
-    return result;
-    }
-
-    Rational operator/(const Rational&right){
-    Rational result(num*right.den, den*right.num);
-    return result;
-    }
-
-
-
-   int getNum(){ return num;}
-   int getDen(){ return den;}
-
-};
-
-void print( Rational& r){
-cout << r.getNum() << "/" << r.getDen() << endl;
+    return max;
 }
+int repeated2heads(const string& seq)
+{
+    int cnt;
+    int total=0;
+    for(unsigned int i=0; i<seq.size(); i++)
+    {
+
+        if(seq[i]=='H')
+        {
+            cnt=0;
+            while(seq[i]=='H')
+            {
+                cnt++;
+                i++;
+            }
+            if(cnt==2)
+            {
+                total++;
+            }
+        }
+
+    }
+    return total;
+}
+
+char randmChoice()
+{
+    srand(time(0));
+    char r = (rand() % 2) ? 'T' : 'H';
+    return r;
+}
+
+void randm1000()
+{
+    srand(time(0));
+    int tailCnt=0;
+    int headCnt=0;
+    float ratio;
+
+    for(int i=0; i<1000; i++)
+    {
+        char r = (rand() % 2) ? 'T' : 'H';
+        cout<< r;
+        if(r=='H')
+        {
+            headCnt++;
+        }
+        else
+        {
+            tailCnt++;
+        }
+    }
+    cout<< endl;
+    ratio=float(headCnt)/float(tailCnt);
+    cout << "Heads: "<<headCnt << endl;
+    cout << "Tails: " << tailCnt
+         << endl;
+    cout << "Ratio: " << ratio << endl;
+}
+
 int main()
 {
-Rational r1(2,2);
-Rational r2(1,2);
-Rational r= r1 * r2;
-cout << "multiplication: ";
- print(r);
-r=r1/r2;
-cout << "division: ";
- print(r);
-r=r1+r2;
-cout << "addition: ";
-print(r);
+    srand(time(0));
 
+    char input;
+    cout << "Guess, (T) or (H)" << endl;
+    cin>>input;
+    if(randmChoice() == input)
+    {
+        cout << "Your guess is correct!!!" << endl;
+    }
+    else
+    {
+        cout << "Comp choice: "<<randmChoice() << endl;
+    }
+    randm1000();
+    cout << repeated2heads("THHHHTTHHTHHHTHH") <<endl;
+    cout << longestheads("THHHHTTHHTHHHTHH") << endl;
     return 0;
 }
