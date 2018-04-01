ec9bb0f14f451fa2ebf875b01bf68db4aa56bc9b
Nazerke Seidan <33206532+NazerkeBS@users.noreply.github.com>
2018-04-01 12:40:42 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,256 +1,67 @@
 #include <iostream>
-#include <fstream>
-#include <cstdlib>
 
 using namespace std;
+// to use private variables of another class: friend
 
-
-class Creature
-{
-protected:
-    string name;
-    int distance;
-    int water;
-public:
-    Creature(const string &name, int water)
-    {
-        this->name=name;
-        this->water = water;
-        this->distance = 0;
-    }
-
-    bool isAlive()
-    {
-        return water>0;
-    }
-
-    virtual void consume(char dayType)=0;
-    virtual void step(char dayType)=0;
-
-    int getDistance()
-    {
-        return distance;
-    }
-    string getName()
-    {
-        return name;
-    }
-};
-
-class Sandwalker : public Creature
-{
-public:
-    Sandwalker(const string &name, int water) : Creature(name,water)
-    {
-    }
+class Rational{
 private:
-    void consume(char dayType)
-    {
-        if(isAlive())
-        {
-            switch(dayType)
-            {
-            case 's':
-                water-=1;
-            case 'c':
-                water+=0;
-            case 'r':
-                water+=3;
-            }
-        }
-
-        if(water>8)
-        {
-            water=8;
-        }
-
-    }
-    void step(char dayType)
-    {
-        if(isAlive())
-        {
-            switch(dayType)
-            {
-            case 's' :
-                distance+=3;
-            case 'c' :
-                distance+=1;
-            case 'r' :
-                distance+=0;
-            }
-
-        }
-    }
-
-};
-
-class Sponge : public Creature
-{
-
+    int num;
+    int den;
 public:
-    Sponge(const string &name, int water) : Creature(name,water) {}
-private:
-    void consume(char dayType)
-    {
-        if(isAlive())
-        {
-            switch(dayType)
-            {
-            case 's':
-                water-=4;
-            case 'c':
-                water-=1;
-            case 'r':
-                water+=6;
-            }
-
-        }
-        if(water > 20 )
-        {
-            water=20;
-        }
+    Rational(int num=0, int den=1){    // default constructor
+    this->num=num;
+    if(den != 0){
+    this->den=den;
+    }
+    }
+    Rational(const Rational& other) : num(other.num),den(other.den){
+    // copy constructor
+    }
+    Rational& operator=(const Rational& other){
+    // assignment
+    num=other.num;
+    den=other.den;
+    return *this;
     }
 
-    void step(char dayType)
-    {
-        if(isAlive())
-        {
-            switch(dayType)
-            {
-            case 's' :
-                distance+=0;
-            case 'c' :
-                distance+=1;
-            case 'r' :
-                distance +=3;
-
-            }
-        }
-
+    Rational operator*(const Rational& right){
+    Rational result(num*right.num, den*right.den);
+    return result;
     }
 
+    Rational operator+(const Rational&right){
+    Rational result(num*right.den+den*right.num, den*right.den);
+    return result;
+    }
+
+    Rational operator/(const Rational&right){
+    Rational result(num*right.den, den*right.num);
+    return result;
+    }
+
+
+
+   int getNum(){ return num;}
+   int getDen(){ return den;}
 
 };
 
-class Wanderer : public Creature
-{
-public:
-    Wanderer(const string &name, int water) : Creature(name,water) {}
-private:
-    void consume(char dayType)
-    {
-        if(isAlive())
-        {
-            switch(dayType)
-            {
-            case 's':
-                water -=2;
-            case 'c':
-                water -=1;
-            case 'r':
-                water +=3;
-            }
-        }
-
-        if(water > 12)
-        {
-            water=12;
-        }
-    }
-
-    void step(char dayType)
-    {
-        if(isAlive())
-        {
-            switch(dayType)
-            {
-            case 's':
-                distance +=1;
-            case 'c':
-                distance +=2;
-            case 'r':
-                distance +=1;
-            }
-        }
-
-    }
-};
-
-
-Creature* findWinner(Creature* creatures[], int size)
-{
-    Creature* winner=0;
-    for(int i=0; i<size; i++)
-    {
-
-        if((creatures[i]->isAlive()) || (winner==0) && creatures[i]->getDistance() > winner->getDistance())
-        {
-            winner=creatures[i];
-        }
-    }
-    return winner;
+void print( Rational& r){
+cout << r.getNum() << "/" << r.getDen() << endl;
 }
-
 int main()
 {
+Rational r1(2,2);
+Rational r2(1,2);
+Rational r= r1 * r2;
+cout << "multiplication: ";
+ print(r);
+r=r1/r2;
+cout << "division: ";
+ print(r);
+r=r1+r2;
+cout << "addition: ";
+print(r);
 
-    Creature* creatures[4];
-    ifstream file("creatures.txt");
-
-    if(!file.is_open())
-    {
-        cout << "file can't be opened." << endl;
-        exit(1);
-    }
-    int cntLines;
-    file >> cntLines;
-    for(int i=0; i<cntLines; i++)
-    {
-        string name,type;
-        int water;
-        file >> name >> type >> water;
-        if(type=="sandwalker")
-        {
-            creatures[i]= new Sandwalker(name,water);
-        }
-        else if(type=="wanderer")
-        {
-            creatures[i]=new Wanderer(name,water);
-        }
-        else if(type=="sponge")
-        {
-            creatures[i]=new Sponge(name,water);
-        }
-
-    }
-
-    string dayTypes;
-    file >> dayTypes;
-    file.close();
-
-    for(unsigned int j=0; j<dayTypes.size(); j++)
-    {
-        for(int i=0; i<cntLines; i++)
-        {
-            creatures[i]->consume(dayTypes[j]);
-            creatures[i]->step(dayTypes[j]);
-        }
-
-    }
-
-    Creature* winner = findWinner(creatures, cntLines);
-    if(winner==0)
-    {
-        cout << "everyone died" << endl;
-    }
-    else
-    {
-        cout <<"Winner: " <<winner->getName() << endl;
-    }
-
-    for(int i=0; i<cntLines; i++)
-    {
-        delete creatures[i];
-    }
     return 0;
 }
